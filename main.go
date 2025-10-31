567bb193cf4764e6bc9385db92b0d5a95383ba72
Keith Thomson <camelbyte@proton.me>
2025-10-30 22:52:02 -0400

    application is working. ready to use as template.

--- main.go
+++ main.go
@@ -10,26 +10,19 @@ import (
 )
 
 func main() {
-	// Create a new Fiber instance
 	app := fiber.New(fiber.Config{
 		AppName: "EvBagel",
 	})
 
-	// Middleware
 	app.Use(logger.New())
 	app.Use(recover.New())
 
-	// Serve static files
-	app.Static("/static", "./static")
-
-	// Routes
 	app.Get("/", handlers.HomeHandler)
 	app.Get("/about", handlers.AboutHandler)
 	app.Get("/contact", handlers.ContactHandler)
 
-	// Start server
 	log.Println("Starting server on :3000")
-	if err := app.Listen(":3000"); err != nil {
+	if err := app.Listen(":8080"); err != nil {
 		log.Fatal(err)
 	}
 }
